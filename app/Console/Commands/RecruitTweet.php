ab7bc5870a7d45ee39c290661682bd77adaf0c5a
kurosawa <kurosawa0626+github@gmail.com>
2023-02-13 09:32:33 +0900

    twitter apiを使わないように変更

--- app/Console/Commands/RecruitTweet.php
+++ app/Console/Commands/RecruitTweet.php
@@ -46,47 +46,26 @@ class RecruitTweet extends Command
         $delete_time = (new Carbon())->subWeek();
         Tweet::where('tweeted_at', '<', $delete_time)->delete();
 
-        // TwitterAPIが最大100件なので100件で分割する
-        $chunk_tweets = Tweet::all()->chunk(100);
-        foreach ($chunk_tweets as $chunk_tweet) {
-            $tweet_ids = array_column($chunk_tweet->toArray(), 'tweet_id');
-            $url = 'https://api.twitter.com/2/tweets?ids='.(implode(',', $tweet_ids)).'&tweet.fields=created_at';
+        foreach (Tweet::all() as $tweet) {
+            $url = 'https://publish.twitter.com/oembed?url=https://twitter.com/'.$tweet->username.'/status/'.$tweet->tweet_id;
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization:Bearer '.Config::get('twitter.bearer_token')]);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($curl);
             curl_close($curl);
             $response = json_decode($response);
-
-            // 削除済み、鍵垢を削除
-            Log::info("削除済み、鍵垢のツイートを削除");
-            if (isset($response->errors)) {
-                $delete_ids = array_column($response->errors, 'value');
-                $delete_count = Tweet::whereIn('tweet_id', $delete_ids)->delete();
-                Log::info("Errors: ".$delete_count);
+            if (!$response) {
+                Log::info("削除済み、鍵垢のツイートを削除");
+                Tweet::where('tweet_id', $tweet->tweet_id)->delete();
+                continue;
             }
 
-            // データないツイートのデータを取得
-            Log::info("データ取得");
-            if (isset($response->data)) {
-                foreach ($response->data as $data) {
-                    $tweet = Tweet::where('tweet_id', $data->id)->whereNull('text')->first();
-                    if ($tweet) {
-                        $url = 'https://publish.twitter.com/oembed?url=https://twitter.com/'.$tweet->username.'/status/'.$tweet->tweet_id;
-                        $curl = curl_init($url);
-                        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-                        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                        $response = curl_exec($curl);
-                        curl_close($curl);
-
-                        $tweet->fill([
-                            'html' => json_decode($response)->html
-                        ])->save();
-                    }
-                }
+            if (!$tweet->html) {
+                Log::info("削除済み、鍵垢のツイートを削除");
+                $tweet->fill([
+                    'html' => json_decode($response)->html
+                ])->save();
             }
         }
     }
